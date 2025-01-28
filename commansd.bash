8fa1f58ed87eb8d79961fcf32c93197a9b648a84
David Mayo <dcmayo@gmail.com>
2025-01-28 15:22:22 -0500

    flags

--- commansd.bash
+++ commansd.bash
@@ -1,14 +1,11 @@
-# HOSTNAME
-hostnamectl set-hostname your-new-hostname
-
 # UV
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv python install 3.13 3.12 3.11 3.10 3.9 3.8 3.7
 
 # PYTHON 2
 cd ~/Downloads
-sudo dnf groupinstall "Development Tools"
-sudo dnf install openssl-devel bzip2-devel libffi-devel
+sudo dnf groupinstall "Development Tools" -y
+sudo dnf install openssl-devel bzip2-devel libffi-devel -y
 curl -O https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 tar -xf Python-2.7.18.tgz
 cd Python-2.7.18
@@ -24,8 +21,9 @@ sudo /usr/local/bin/python2.7 -m pip install virtualenv
 # MANUAL
 
 # VSCode
-# Download MANUALLY
-sudo dnf install ~/Downloads/code...rpm
+cd ~/Downloads
+wget https://update.code.visualstudio.com/1.96.4/linux-rpm-x64/stable
+sudo dnf install ~/Downloads/code-1.96.4-1648034190.el8.x86_64.rpm -y
 
 # REQUIRE VENV (Install Python2.7 first)
 python -m pip config set global.require-virtualenv True
